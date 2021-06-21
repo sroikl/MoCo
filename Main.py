ff180a3dc319720773e10bd7ed52ef3b5d2c9495
Ehud Tal <84499506+EhudTal88@users.noreply.github.com>
2021-06-21 22:36:14 +0300

    Add files via upload

--- Main.py
+++ Main.py
@@ -90,5 +90,5 @@ if __name__ == '__main__':
                      device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
                      )
 
-    # TrainMocoContrastive(ConfigFile)
-    TrainDown_stream_task(ConfigFile)
+    TrainMocoContrastive(ConfigFile)
+    # TrainDown_stream_task(ConfigFile)
